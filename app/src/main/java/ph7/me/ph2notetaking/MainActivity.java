c44c78a32bb235354b7d9c1ca9b00d4519e2d90d
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-10-17 20:50:01 +0100

    Improvements, add authority, moved getWritableDatabase to NoteProvider

--- app/src/main/java/ph7/me/ph2notetaking/MainActivity.java
+++ app/src/main/java/ph7/me/ph2notetaking/MainActivity.java
@@ -1,6 +1,5 @@
 package ph7.me.ph2notetaking;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,12 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        createDatabase();
     }
 
-    protected void createDatabase() {
-        DBOpenHelper dbHelper = new DBOpenHelper(this);
-        dbHelper.getWritableDatabase();
-    }
+
 }
---
+++ app/src/main/java/ph7/me/ph2notetaking/NoteProvider.java
@@ -0,0 +1,69 @@
+/**
+ * Created by Pierre-Henry Soria on 17/10/2016.
+ */
+package ph7.me.ph2notetaking;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+public class NoteProvider extends ContentProvider {
+
+    // Authority is the unique name for the app
+    private static final String AUTHORITY = "ph7.me.ph2notetaking.noteprovider";
+    private static final String BASE_PATH = "note";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+
+    // Constant to identify the requested operation
+    private static final int NOTE = 1;
+    private static final int NOTE_ID = 2;
+
+    private SQLiteDatabase database;
+
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTE);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", NOTE_ID);
+    }
+
+    @Override
+    public boolean onCreate() {
+        DBOpenHelper dbHelper = new DBOpenHelper(getContext());
+        database = dbHelper.getWritableDatabase();
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
